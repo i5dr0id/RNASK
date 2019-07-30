cbedf9b61d622d043cdb4be3dbe141f8e9e18876
israelugbodaga <israelugbodaga@gmail.com>
2019-07-30 23:19:31 +0100

    update splashscreen

--- android/app/src/main/java/com/rnask/SplashActivity.java
+++ android/app/src/main/java/com/rnask/SplashActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-
-       Intent intent = new Intent(this, MainActivity.class);
-       startActivity(intent);
-       finish();
-   }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// Window window = activity.getWindow();
+		// Activity activity;
+		// activity.getWindow().setStatusBarColor(activity.getResources().getColor(R.color.blue));
+		// window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+		// window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+		// window.setStatusBarColor(activity.getResources().getColor(R.color.blue));
+		super.onCreate(savedInstanceState);
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+		finish();
+	}
 }
